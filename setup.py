c85525598fca67d27f45c7995b6d47d2ad5b3969
Andrew James Morgan <andyofmelbourne@gmail.com>
2020-01-23 15:25:38 +1100

    version bump

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
-#from Cython.Build import cythonize
-
-import glob
 
 extensions = []
-#extensions.append(Extension( "speckle_tracking.feature_matching", 
-#                            ["speckle_tracking/feature_matching.pyx",]))
-#extensions.append(Extension( "speckle_tracking.poly_utils", 
-#                            ["speckle_tracking/poly_utils.pyx",]))
 
 setup(
     name                 = "speckle-tracking",
-    version              = "2019.0",
+    version              = "2020.0",
     packages             = find_packages(),
     install_requires     = ['pyqtgraph', 'h5py', 'scipy', 'numpy', 'tqdm'],
     ext_modules          = extensions
