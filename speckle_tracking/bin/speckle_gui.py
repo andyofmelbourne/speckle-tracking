ad51a076bbe1b4b048fcea46432da235dde415ba
Andrew James Morgan <andyofmelbourne@gmail.com>
2020-02-06 15:34:07 +1100

    added additional menus

--- speckle_tracking/bin/speckle_gui.py
+++ speckle_tracking/bin/speckle_gui.py
@@ -66,7 +66,7 @@ class Speckle_gui(QMainWindow):
         load_sfs_widget.triggered.connect( load_sfs_action ) 
         dis_menu.addAction(load_sfs_widget)
         load_sfs_action(fnam)
-
+        
         # view h5 widget
         ################
         load_view_h5_data_widget = QAction("view h5 data widget", self)
@@ -74,22 +74,8 @@ class Speckle_gui(QMainWindow):
         load_view_h5_data_widget.triggered.connect( load_view_h5_data_action ) 
         dis_menu.addAction(load_view_h5_data_widget)
         
-        pro_menu = menu.addMenu('Process')
-        
         script_names = []
-        load_pro_widgets = []
-        load_pro_actions = []
-            
-        # mask maker widget
-        ###################
-        script_names.append('mask maker')
-        load_pro_widgets.append(QAction(script_names[-1], self))
-        load_pro_actions.append(lambda x, s = script_names[-1], 
-                                f = fnam : tabs_widget.addTab( \
-                                widgets.Mask_maker_widget(f, params['data_paths'], params['mask_paths']), s))
-        load_pro_widgets[-1].triggered.connect( load_pro_actions[-1] )
-        pro_menu.addAction(load_pro_widgets[-1])
-        
+
         # update_pixel_map widget
         #########################
         #script_names.append('update_pixel_map')
@@ -114,14 +100,93 @@ class Speckle_gui(QMainWindow):
         #                        f = fnam : tabs_widget.addTab(widgets.Fit_defocus_widget(s, f), s))
         #load_pro_widgets[-1].triggered.connect( load_pro_actions[-1] )
         #pro_menu.addAction(load_pro_widgets[-1])
+
+        # populate the Initialisation menu
+        ##################################
+        init_menu = menu.addMenu('Initialisation')
         
-        # auto populate the process menu
+        load_init_widgets = []
+        load_init_actions = []
+
+        # mask maker widget
+        ###################
+        script_names.append('mask maker')
+        load_init_widgets.append(QAction(script_names[-1], self))
+        load_init_actions.append(lambda x, s = script_names[-1], 
+                                f = fnam : tabs_widget.addTab( \
+                                widgets.Mask_maker_widget(f, params['data_paths'], params['mask_paths']), s))
+        load_init_widgets[-1].triggered.connect( load_init_actions[-1] )
+        init_menu.addAction(load_init_widgets[-1])
+        
+        # auto populate from list
+        init_fnams = "make_mask.py make_whitefield.py guess_roi.py fit_defocus_registration.py fit_thon_rings.py generate_pixel_map.py".split()
+        
+        for init_fnam in init_fnams :
+            print(init_fnam)
+            script_name = os.path.split(init_fnam)[1][:-3]
+            
+            mpi = False 
+            
+            script_names.append(script_name)
+            load_init_widgets.append(QAction(script_name, self))
+            load_init_actions.append(lambda x, s = script_name, f = fnam : tabs_widget.addTab(widgets.Auto_build_widget(s, f, mpi=mpi), s))
+            load_init_widgets[-1].triggered.connect( load_init_actions[-1] )
+            init_menu.addAction(load_init_widgets[-1])
+        
+        # populate the Main Loop menu
+        ##################################
+        main_menu = menu.addMenu('Main Loop')
+        
+        # auto populate from list
+        main_fnams = "make_reference.py update_pixel_map.py calc_error.py update_translations.py".split()
+        
+        load_main_widgets = []
+        load_main_actions = []
+        for main_fnam in main_fnams :
+            print(main_fnam)
+            script_name = os.path.split(main_fnam)[1][:-3]
+            
+            mpi = False 
+            
+            script_names.append(script_name)
+            load_main_widgets.append(QAction(script_name, self))
+            load_main_actions.append(lambda x, s = script_name, f = fnam : tabs_widget.addTab(widgets.Auto_build_widget(s, f, mpi=mpi), s))
+            load_main_widgets[-1].triggered.connect( load_main_actions[-1] )
+            main_menu.addAction(load_main_widgets[-1])
+
+
+        # populate the Additional Analysis menu
+        ##################################
+        add_menu = menu.addMenu('Additional Analysis')
+        
+        # auto populate from list
+        add_fnams = "calculate_phase.py calculate_sample_thickness.py focus_profile.py split_half_recon.py zernike.py".split()
+        
+        load_add_widgets = []
+        load_add_actions = []
+        for add_fnam in add_fnams :
+            print(add_fnam)
+            script_name = os.path.split(add_fnam)[1][:-3]
+            
+            mpi = False 
+            
+            script_names.append(script_name)
+            load_add_widgets.append(QAction(script_name, self))
+            load_add_actions.append(lambda x, s = script_name, f = fnam : tabs_widget.addTab(widgets.Auto_build_widget(s, f, mpi=mpi), s))
+            load_add_widgets[-1].triggered.connect( load_add_actions[-1] )
+            add_menu.addAction(load_add_widgets[-1])
+        
+        # auto populate the misc menu
         ################################
-        pro_fnams = glob.glob(root+'/bin/*.py')
-        exclude = ['write_h5.py', 'wipe_cxi.py', 'speckle_gui.py', 'hdf_display.py']
-        #pro_fnams = "fit_defocus_registration.py forward_sim.py make_whitefield.py speckle-gui.py update_pixel_map.py fit_defocus_thon.py h5_operations.py  pos_refine.py stitch.py zernike.py".split()
+        misc_menu = menu.addMenu('Misc')
         
-        for pfnam in pro_fnams :
+        load_misc_widgets = []
+        load_misc_actions = []
+            
+        misc_fnams = glob.glob(root+'/bin/*.py')
+        exclude = ['write_h5.py', 'wipe_cxi.py', 'speckle_gui.py', 'hdf_display.py']
+        
+        for pfnam in misc_fnams :
             print(pfnam)
             if np.any([e in pfnam for e in exclude]) :
                 continue 
@@ -133,11 +198,12 @@ class Speckle_gui(QMainWindow):
             
             mpi = False 
             
-            load_pro_widgets.append(QAction(script_name, self))
-            load_pro_actions.append(lambda x, s = script_name, f = fnam : tabs_widget.addTab(widgets.Auto_build_widget(s, f, mpi=mpi), s))
-            load_pro_widgets[-1].triggered.connect( load_pro_actions[-1] )
-            pro_menu.addAction(load_pro_widgets[-1])
-
+            script_names.append(script_name)
+            load_misc_widgets.append(QAction(script_name, self))
+            load_misc_actions.append(lambda x, s = script_name, f = fnam : tabs_widget.addTab(widgets.Auto_build_widget(s, f, mpi=mpi), s))
+            load_misc_widgets[-1].triggered.connect( load_misc_actions[-1] )
+            misc_menu.addAction(load_misc_widgets[-1])
+        
         self.show()
         
     def autoTab(self, script_name, fnam):
