a1c8bc52d30e51482d87f1865f384ff0f9b12d0a
Andrew James Morgan <andyofmelbourne@gmail.com>
2020-02-07 15:53:35 +1100

    better pixel_map_from_data by calling cmdline

--- speckle_tracking/bin/pixel_map_from_data.py
+++ speckle_tracking/bin/pixel_map_from_data.py
@@ -15,17 +15,55 @@ if __name__ == '__main__':
     # extract the first paragraph from the doc string
     des = st.pixel_map_from_data.__doc__.split('\n\n')[0]
     
-    # now load the necessary data
-    args, params = cmdline_config_cxi_reader.get_all(sc, des, config_dirs=config_dirs, roi=True)
+    # get command line args and config params
+    args, params = cmdline_parser.parse_cmdline_args(sc, des, config_dirs=config_dirs)
     params = params['pixel_map_from_data']
     
-    u, res  = st.pixel_map_from_data(
-                         params['data'].astype(np.float32), params['xy_pix'],
-                         params['whitefield'], params['mask'],
-                         search_window = params['search_window'])
+    # Initialisation
+    #---------------
+    # make mask
+    if params['mask'] == 'auto':
+        os.system("make_mask.py " + args.filename)
+
+    # make whitefield
+    if params['whitefield'] == 'auto':
+        os.system("make_whitefield.py " + args.filename)
+                       
+    # make ROI
+    if params['roi'] == 'auto':
+        os.system("guess_roi.py " + args.filename)
+
+    # defocus
+    if params['defocus'] == 'auto':
+        os.system("fit_thon_rings.py " + args.filename)
+
+    # pixel map
+    if params['pixel_map'] == 'auto':
+        os.system("generate_pixel_map.py " + args.filename)
+     
+    # Main Loop
+    #----------
+    for i in range(3):
+        # make reference
+        os.system("make_reference.py " + args.filename)
+        
+        # update pixel map
+        os.system("update_pixel_map.py " + args.filename)
+        
+        # update translations
+        os.system("update_translations.py " + args.filename)
+        
+        # calculate error
+        os.system("calc_error.py " + args.filename)
     
-    out = { 
-            'pixel_map'   : u,
-            'object_map'  : res['object_map']
-          }
-    cmdline_config_cxi_reader.write_all(params, args.filename, out, apply_roi=True)
+    # Adiational analysis
+    #--------------------
+    # calculate sample thickness
+    os.system("calculate_sample_thickness.py " + args.filename)
+    
+    # calculate phase
+    os.system("calculate_phase.py " + args.filename)
+
+    # calculate focus profile
+    os.system("focus_profile.py " + args.filename)
+
