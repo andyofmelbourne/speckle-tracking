96737586660d208a143790e0b2facab68dc0ea38
Andrew James Morgan <andyofmelbourne@gmail.com>
2020-02-07 12:25:17 +1100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name                 = "speckle-tracking",
-    version              = "2019.0",
+    version              = "2019.1",
     packages             = find_packages(),
     scripts              = glob.glob('speckle_tracking/bin/*.py')
     )
