410fec297191527ec6967d3db4556feb8dfc657c
Nikolay Ivanov <simply_nicky@nikolays-mbp.localdomain>
2020-12-03 15:39:40 +0100

    gitignore and setup updated

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 import glob
+import sys
+import os
 
-setup(
-    name                 = "speckle-tracking",
-    version              = "2020.1",
-    packages             = find_packages(),
-    scripts              = glob.glob('speckle_tracking/bin/*.py')
-    )
+with open('README.rst', 'r') as readme:
+      long_description = readme.read()
+
+setup(name='speckle-tracking',
+      version='2020.1',
+      long_description=long_description,
+      long_description_content_type='text/rst',
+      url='https://github.com/andyofmelbourne/speckle-tracking',
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={'speckle_tracking': ['*.cl', 'bin/*.ini', '*.pyx', '*.c']},
+      install_requires=['numpy'],
+      scripts=glob.glob('speckle_tracking/bin/*.py'))
